5a24ed8cc0824618138a0f7edd8034536e420942
Ritchai <156800910+RitcBoss@users.noreply.github.com>
2024-10-22 15:34:49 +0700

    Update db.js

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'password',
+  password: '', //password
   database: 'income_expense_db'
 });
 
